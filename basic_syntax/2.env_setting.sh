b43f141cf0138f6ceb09eb949f315b598bdb0c93
yool <jungyooree@gmail.com>
2025-04-08 00:17:46 +0900

    title1
    title의 수정사항은...입니다

--- basic_syntax/2.env_setting.sh
+++ basic_syntax/2.env_setting.sh
@@ -52,3 +52,8 @@ git config -local user.email '유저이메일'
 git config user.name
 git config user.email
 git config --list
+
+
+# 특정 파일을 git 축적 목록에서 제외시키고 싶다면 .gitignore 파일에 파일목록 등록
+# 이미 add, commit 되어 버린 파일을 추적목록에서 제외하고 싶다면
+git rm -r --cached .
