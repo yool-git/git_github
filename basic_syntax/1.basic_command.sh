27786651d98f73e6be106d348ba23f5c3b0eacb6
yool <jungyooree@gmail.com>
2025-04-08 20:24:16 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -36,3 +36,10 @@ git log --all
 
 # 충돌무시하고강제 push 할때
 git push origin main --force
+
+# 특정 commit ID로의 전환(소스코드를 과거의 특정 버전으로 되돌릴때)
+git checkout 커밋ID
+
+# 다시 현재의 브랜치의 commit으로 되돌아올때
+git checkout 현재브랜치명(main)
+
