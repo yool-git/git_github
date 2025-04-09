a9f99ba7462235e84a7ceafdf6a4a527362b9000
yool <jungyooree@gmail.com>
2025-04-10 00:14:33 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -74,3 +74,14 @@ git stash list
 # 저장한 목록 모두 삭제
 git stash clear
 
+# 버전명시
+git tag v1.0
+git tag -a v1.0 -m '1.주요사항A 2.주요사항B'
+
+# tag release 배포
+# commit , push와 상관없이 태그별도로 push해줘야 한다.
+git push origin  v1.0
+
+# 태그 목록조회
+git tag
+
