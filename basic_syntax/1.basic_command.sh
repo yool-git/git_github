e06da4a56bc00d8d524f6a9be230585b8db0bae2
yool <jungyooree@gmail.com>
2025-04-09 23:30:05 +0900

    fix

--- basic_syntax/1.basic_command.sh
+++ basic_syntax/1.basic_command.sh
@@ -63,11 +63,14 @@ git push origin main
 
 # 작업중인 사항을 임시저장(충돌사항에서 많이 사용)
 git stash
-
-# 저장한 최신의 작업목록을 꺼내기
+# 저장한 최신의 작업목록을 꺼내서 적용하기기
 git stash pop
-
-
-
-
+# 임시저장한 목록은 그대로 놔둔채 특정 index항목 적용
+git stash apply 인덱스번호
+# 임시저장한 작업목록 내용 조회
+git stash show -p 인덱스번호
+# 임시저장한 작업목록 조회
+git stash list
+# 저장한 목록 모두 삭제
+git stash clear
 
